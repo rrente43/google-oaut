004f4fef5124e369dc921d4893ae5aceef1239d2
Rubi Renteria <rubirenteria43@gmail.com>
2019-10-19 22:05:05 -0600

    added google auth routes

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
+const authRoutes =require('./routes/auth-routes');
 
 app.set('view engine','ejs');
+
+//setting up routes
+app.use('/auth', authRoutes);
 
 app.get('/',(req,res) =>{
     res.render('home');
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -5,9 +5,13 @@ router.get('/login',(req,res)=>{
     res.render('login');
 });
 
-router.get('/logout')
+router.get('/logout', (req,res)=>{
+    //handle with passport
+    res.send("logging out");
+});
 //auth google
-router.ger('/google',(req,res)=>{
+router.get('/google',(req,res)=>{
     //handle with passport
     res.send('loggin with google');
 });
+module.exports = router;
