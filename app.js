8ddc49571c1e6004e2683bac5d61570a5f15cd1a
Rubi Renteria <rubirenteria43@gmail.com>
2019-10-20 20:13:47 -0600

    setting up a const

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const authRoutes =require('./routes/auth-routes');
+const passportSetup = require('./config/passport-setup');
 
 app.set('view engine','ejs');
 
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const passport = require('passport');
 
 //auth login
 router.get('/login',(req,res)=>{
@@ -10,8 +11,7 @@ router.get('/logout', (req,res)=>{
     res.send("logging out");
 });
 //auth google
-router.get('/google',(req,res)=>{
-    //handle with passport
-    res.send('loggin with google');
-});
+router.get('/google', passport.authenticate('google',{
+    scope: ['profile']
+}));
 module.exports = router;
